0d77a9d858e3502b6dfee87ffebcf0dfbdd6c1c1
Arvind Kumar <thakurarvindkr10@gmail.com>
2023-09-07 00:06:10 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
 import EmailList from "./components/EmailList";
 import Compose from "./components/Compose";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { selectSendMessageIsOpen } from "./features/mailSlice";
 import EmailDetails from "./components/EmailDetails";
 import { Route, Routes } from "react-router-dom";
 import Login from "./Pages/Login";
-import { userSelect } from "./features/userSlice";
+import { signIn, signOut, userSelect } from "./features/userSlice";
+import { auth } from "./configFirebase/Firebase";
 
 const App = () => {
   const isMessageOpen = useSelector(selectSendMessageIsOpen);
   let user = useSelector(userSelect);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        dispatch(
+          signIn({
+            displayName: user?.displayName,
+            photoURL: user?.photoURL,
+            email: user?.email,
+          })
+        );
+      } else {
+        dispatch(signOut());
+      }
+    });
+  }, []);
+
   return (
     <>
       {user ? (
