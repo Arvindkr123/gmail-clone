48d2f8b37cdab4859231d0324fd9c7a4bb44d70e
Arvind Kumar <thakurarvindkr10@gmail.com>
2023-09-06 17:42:15 +0530

    read the data from firebase

--- src/components/EmailList.jsx
+++ src/components/EmailList.jsx
@@ -1,51 +1,54 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./emailList.css";
 import EmailListSetting from "./EmailListSetting";
 import EmailType from "./EmailType";
 import EmailBody from "./EmailBody";
+import { db } from "../configFirebase/Firebase";
+import { collection, getDocs } from "firebase/firestore";
 
 const EmailList = () => {
+  const [emails, setEmails] = useState([]);
+  console.log(emails);
+
+  // Function to read data from the "mails" collection
+  const readDataFromFirestore = async () => {
+    try {
+      const mailsCollectionRef = collection(db, "mails");
+      const querySnapshot = await getDocs(mailsCollectionRef);
+
+      const emailList = [];
+      querySnapshot.forEach((doc) => {
+        const data = doc.data();
+        const emailWithId = { id: doc.id, ...data };
+        emailList.push(emailWithId);
+      });
+
+      // Set the emails state with the list of emails including IDs
+      setEmails(emailList);
+    } catch (error) {
+      console.error("Error reading data from Firestore:", error);
+    }
+  };
+
+  useEffect(() => {
+    // Call the function to read data when the component mounts
+    readDataFromFirestore();
+  }, []); // Empty dependency array to ensure it runs once
+
   return (
     <div className="emailList">
       <EmailListSetting />
       <EmailType />
 
-      <EmailBody
-        name="Elon Musk"
-        subject="This is important message"
-        message="Lorem ipsum dolor sit amet consectetur adipisicing elit. At voluptatibus quae Lorem ipsum dolor sit amet consectetur adipisicing elit. At voluptatibus quae Lorem ipsum dolor sit amet consectetur adipisicing elit. At voluptatibus quae "
-        time="2:30 PM"
-      />
-      <EmailBody
-        name="Elon Musk"
-        subject="This is important message"
-        message="Lorem ipsum dolor sit amet consectetur adipisicing elit. At voluptatibus quae Lorem ipsum dolor sit amet consectetur adipisicing elit. At voluptatibus quae Lorem ipsum dolor sit amet consectetur adipisicing elit. At voluptatibus quae "
-        time="2:30 PM"
-      />
-      <EmailBody
-        name="Elon Musk"
-        subject="This is important message"
-        message="Lorem ipsum dolor sit amet consectetur adipisicing elit. At voluptatibus quae Lorem ipsum dolor sit amet consectetur adipisicing elit. At voluptatibus quae Lorem ipsum dolor sit amet consectetur adipisicing elit. At voluptatibus quae "
-        time="2:30 PM"
-      />
-      <EmailBody
-        name="Elon Musk"
-        subject="This is important message"
-        message="Lorem ipsum dolor sit amet consectetur adipisicing elit. At voluptatibus quae Lorem ipsum dolor sit amet consectetur adipisicing elit. At voluptatibus quae Lorem ipsum dolor sit amet consectetur adipisicing elit. At voluptatibus quae "
-        time="2:30 PM"
-      />
-      <EmailBody
-        name="Elon Musk"
-        subject="This is important message"
-        message="Lorem ipsum dolor sit amet consectetur adipisicing elit. At voluptatibus quae Lorem ipsum dolor sit amet consectetur adipisicing elit. At voluptatibus quae Lorem ipsum dolor sit amet consectetur adipisicing elit. At voluptatibus quae "
-        time="2:30 PM"
-      />
-      <EmailBody
-        name="Elon Musk"
-        subject="This is important message"
-        message="Lorem ipsum dolor sit amet consectetur adipisicing elit. At voluptatibus quae Lorem ipsum dolor sit amet consectetur adipisicing elit. At voluptatibus quae Lorem ipsum dolor sit amet consectetur adipisicing elit. At voluptatibus quae "
-        time="2:30 PM"
-      />
+      {emails.map((email) => (
+        <EmailBody
+          key={email.id}
+          name={email.to}
+          subject={email.subject}
+          message={email.message}
+          time={new Date(email.timestamp?.seconds * 1000).toLocaleTimeString()}
+        />
+      ))}
     </div>
   );
 };
